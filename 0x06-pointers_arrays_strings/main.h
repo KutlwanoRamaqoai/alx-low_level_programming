a9ec0a8b111e9c7cf064f259b44c44e5ac32098e
KutlwanoRamaqoai <132349219+KutlwanoRamaqoai@users.noreply.github.com>
2023-06-29 11:57:12 +0200

    tasks

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,4 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+/*
+ * This header file has all prototypes
+ */
 char *_strcat(char *dest, char *src);
+
+char *leet(char *);
 
 char *_strncat(char *dest, char *src, int n);
 
@@ -21,3 +28,4 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 
 int _putchar(char c);
+#endif
